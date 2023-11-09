26d4a81b6e9842fccc7aa75ef656ba21fb88bb52
1337 &2 veg <44616505+theoboldalex@users.noreply.github.com>
2023-11-09 22:56:55 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ $dotenv->load();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>Docker PHP Template</title>
+        <title>ｖａｐｏｒ░ｄｒｅａｍ</title>
     </head>
     <body>
         <div>
