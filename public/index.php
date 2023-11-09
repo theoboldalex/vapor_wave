7ba4dea5f893d1529d1e19be3652ce8b7efc7e88
1337 &2 veg <44616505+theoboldalex@users.noreply.github.com>
2023-11-09 23:02:55 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -12,6 +12,33 @@ $dotenv->load();
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <title>ｖａｐｏｒ░ｄｒｅａｍ</title>
+        <style>
+            body {
+              margin: 0;
+              height: 100vh;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              overflow: hidden;
+            }
+            
+            .gradient-container {
+              width: 100%;
+              height: 100%;
+              background: linear-gradient(to right, #ff00ff, #00ff00);
+              background-size: 200% 100%;
+              animation: gradientAnimation 5s linear infinite;
+            }
+            
+            @keyframes gradientAnimation {
+              0% {
+                background-position: 0% 50%;
+              }
+              100% {
+                background-position: 100% 50%;
+              }
+            }
+        </style>
     </head>
     <body>
         <div>
