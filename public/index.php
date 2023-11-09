cbf99ac75b7d5c2430854646ace4da7e1fa1b739
1337 &2 veg <44616505+theoboldalex@users.noreply.github.com>
2023-11-09 23:12:14 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
-// <?php
+<?php
 
-// require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
-// use Dotenv\Dotenv;
+use Dotenv\Dotenv;
 
-// $dotenv = Dotenv::createImmutable(__DIR__.'/../');
-// $dotenv->load();
+$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
 
-// ?>
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
