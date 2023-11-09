519ff36362c42929cfff223b7d8015fe9a6e32f6
1337 &2 veg <44616505+theoboldalex@users.noreply.github.com>
2023-11-09 23:05:07 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -34,8 +34,11 @@ $dotenv->load();
               0% {
                 background-position: 0% 50%;
               }
-              100% {
+              50% {
                 background-position: 100% 50%;
+              }
+              100% {
+                background-position: 0% 50%;
               }
             }
         </style>
